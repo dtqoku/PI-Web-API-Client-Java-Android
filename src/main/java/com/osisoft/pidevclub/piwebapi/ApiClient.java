622f2386a5601501561ea9a9c0f2d6ac417e7291
marcopinheiro <marco.pinheiro@gmail.com>
2018-08-07 10:19:25 +0100

    Update ApiClient.java

--- src/main/java/com/osisoft/pidevclub/piwebapi/ApiClient.java
+++ src/main/java/com/osisoft/pidevclub/piwebapi/ApiClient.java
@@ -616,14 +616,15 @@ public class ApiClient {
      * Sets the connect timeout (in milliseconds).
      * A value of 0 means no timeout, otherwise values must be between 1 and
      *
-     * @param connectionTimeout connection timeout in milliseconds
-     * @return Api client
+     * @param connectionTimeout default connect timeout in seconds. The connectTimeout is applied when connecting a TCP socket to the target host.
+	 * @param writeTimeout default read timeout in seconds. The write timeout is applied for individual write IO operations. 
+	 * @param readTimeout default write timeout in seconds. The read timeout is applied to both the TCP socket and for individual read IO operations including on Source of the Response.
      */
-    public ApiClient setConnectTimeout(int connectionTimeout) {
-        clientBuilder.connectTimeout(connectionTimeout, TimeUnit.SECONDS)
-                .writeTimeout(connectionTimeout, TimeUnit.SECONDS)
-                .readTimeout(connectionTimeout, TimeUnit.SECONDS);
-        return this;
+    public setConnectTimeout(int connectionTimeout, int writeTimeout, int readTimeout) {
+		this.httpClient = new clientBuilder.connectTimeout(connectionTimeout, TimeUnit.SECONDS)
+				.writeTimeout(writeTimeout, TimeUnit.SECONDS)
+				.readTimeout(readTimeout, TimeUnit.SECONDS)
+				.build();
     }
 
     /**
